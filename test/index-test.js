db933ca92bf436819cfc18e956e2d950e82b23c2
OM Miles <olivia.m.miles@gmail.com>
2021-03-16 17:31:11 -0700

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,7 @@
+// enabling the tests to access the variables in index.js
 const index = require("../index.js");
 
+// testing our name variable
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -7,13 +9,14 @@ describe('what-is-a-test', () => {
     })
   })
 
-
+// testing our height variable
   describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
   })
 
+// testing our message variable
   describe('Message', () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
